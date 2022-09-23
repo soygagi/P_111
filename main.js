9a61907a0f71bcd29b9422b3e05f4b19cc8ad39e
soygagi <101847092+soygagi@users.noreply.github.com>
2022-09-22 19:39:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,3 +11,28 @@ function take_snapshot(){
         document.getElementById("result").innerHTML = '<img id="captured_image"captured_image" src="'+data_uri+'"/>';
     });
 }  
+console.log('ml5 version:',ml5.version);
+classifier = m15.imageClassifier('https://teachablemachine.withgoogle.com/models/mJbuaDVe4/model.json',modelLoaded);
+function modelLoaded(){
+    console.log('modelo cargado;')
+}
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data1 = "La primer prediccion es "+prediccion;
+    speak_data2 = "la segunda prediccion es "+prediccion2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
+    synth.speak(utterThis);
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error)
+    }else{
+        console.log(results)
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+    }
+}
